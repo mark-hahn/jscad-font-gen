fd1a35b61436da954e53334400a7032d666d7775
Mark Hahn <mark@hahnca.com>
2022-02-12 13:03:49 -0800

    multiple fonts

--- index.js
+++ index.js
@@ -1,57 +1,74 @@
 // import {parse} from "svg-parser"
-import fs      from 'fs'
-import {getSystemErrorMap} from 'util';
+import fs from 'fs'
 
-const svg = fs.readFileSync(
-              'fonts/Hershey/HersheyGothEnglish.svg').toString();
-
-const exec = (str, regex, name, dbg=false) => {
+const exec1 = (regex, str, name, dbgOk=false, dbgErr=true) => {
   const group = regex.exec(str)?.[1];
   if(group) {
-    if(dbg) console.log(`${name}: "${group}"`);
+    if(dbgOk) console.log(`${name}: "${group}"`);
     return group;
   }
-  console.log(`\nError: "${name}" missing from\n` +
+  if(dbgErr) console.log(`\nError: "${name}" missing from\n` +
               `${str.slice(0,80)} ${str.length > 80 ? ' ...' : ''}`);
-  process.exit();
+  return null;
 }
-
-const reName    = new RegExp(/<font.*?id="(\w+?)".*?>/);
-const reHeight  = new RegExp(/<font-face.*?cap-height="(\d*?)".*?\/>/s);
-const reGlyph   = new RegExp(/<glyph\s+?(.*?)\/>/g);
-const reUnicode = new RegExp(/unicode="(.)"/);
-const reHAdvX   = new RegExp(/horiz-adv-x="(\d*?)"/s);
-const reVec     = new RegExp(
-  /M\s+?(\d+?)\s+?(\d+?)\s+?L\s+?(\d+?)\s+?(\d+?)/s);
-
-const name   =          exec(svg, reName,   'name');
-const height = parseInt(exec(svg, reHeight, 'height'));
-
-let output = `{name:'${name}',height:${height},`;
-// console.log(output);
-
-let glyphExec;
-while (glyphExec = reGlyph.exec(svg)) {
-  const glyph = glyphExec[0];
-  // console.log({glyph});
-
-  const unicode = exec(glyph, reUnicode, 'unicode').charCodeAt(0);
-  const hAdvX   = exec(glyph, reHAdvX, 'horiz-adv-x');
-
-  output += `${unicode}:[${parseInt(hAdvX)},`;
-  reVec.lastIdx = 0
-  let vec;
-  while (vec = reVec.exec(glyph)) {
-    // console.log({vec});
-    const [,startX, startY, endX, endY] = vec;
-    output += `${startX},${startY},${endX},${endY},`;
-    break;
+const exec = (regex, str, name, dbgOk=false, dbgErr=true) => {
+  const groups = regex.exec(str);
+  if(groups) {
+    if(dbgOk) console.log(`${name}: "${groups}"`);
+    return groups;
   }
-  output += '],';
-
-  output += '}';
-
-  console.log(output);
-
-  break;
+  if(dbgErr) console.log(`\nError: "${name}" missing from\n` +
+              `${str.slice(0,80)} ${str.length > 80 ? ' ...' : ''}`);
+  return null;
 }
+
+const reName     = new RegExp(/<font.*?id="(\w+?)".*?>/i);
+const reHeight   = new RegExp(/<font-face.*?cap-height="(\d*?)".*?\/>/is);
+const reGlyph    = new RegExp(/<glyph\s+?(.*?)\/>/igs);
+const reUnicode  = new RegExp(/unicode="(.)"/i);
+const reHAdvX    = new RegExp(/horiz-adv-x="([\d\.]*?)"/is);
+const rePoints   = new RegExp(/d="(.*?)"/igs);
+const rePoint    = new RegExp(/([ML])\s+([\d\.]+)\s+([\d\.]+)/igs);
+
+const fontPaths = ['fonts/Hershey/HersheySans1.svg', 
+                   'fonts/Hershey/HersheyGothEnglish.svg'];
+
+let output = `\nconst fonts = {`
+
+for (let fontPath of fontPaths) { 
+
+  const svg = fs.readFileSync(fontPath).toString();
+
+  const name   = exec1(reName,   svg,   'name');
+  const height = exec1(reHeight, svg, 'height');
+  output += `\n${name}:{height:${height},`;
+
+  let glyph;
+  while (glyph = exec1(reGlyph, svg, 'glyph', false, false)) {
+
+    const unicode = exec1(reUnicode, glyph, 'unicode',false,false);
+    if(!unicode || ![' ','!','W','y','a','t'].includes(unicode)) continue;
+
+    output += `\n${unicode.charCodeAt(0)}:`;
+    output += `[${exec1(reHAdvX, glyph, 'horiz-adv-x')}`;
+
+    const points = exec1(rePoints, glyph, 'points', false, false);
+    if(!points) {
+      output += ',],';
+    }
+    else {
+      let point;
+      rePoint.lastIndex = 0;
+      while (point = exec(rePoint, points, 'point', false, false)) {
+        const [,m,x,y] = point;
+        output += `${m == 'M' ? ',' : ''}${x},${y},`;
+      }
+      if(rePoints.lastIndex = 0) output += ',';
+      output += '],';
+    }
+  }
+  output += '},\n';
+}
+output += '}';
+console.log(output);
+fs.writeFileSync("fonts/output.js", output);

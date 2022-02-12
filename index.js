b9c91da21c3fc1e4173b57a90173930dada6658d
Mark Hahn <mark@hahnca.com>
2022-02-11 17:16:12 -0800

    added exec function

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 // import {parse} from "svg-parser"
 import fs      from 'fs'
+import {getSystemErrorMap} from 'util';
 
 const svg = fs.readFileSync(
               'fonts/Hershey/HersheyGothEnglish.svg').toString();
 
-// const reGlyphNameAdv = 
-//       new RegExp('<glyph\s+?unicode="(.)"\s+?'         +
-//                            'glyph-name="(.*?)"\s+?'    +
-//                            'horiz-adv-x="(\d+?)"','g');
+const exec = (str, regex, name, dbg=false) => {
+  const group = regex.exec(str)?.[1];
+  if(group) {
+    if(dbg) console.log(name + ': ' + group);
+    return group;
+  }
+  console.log(`\nError: "${name}" missing from\n` +
+              `${str.slice(0,80)} ${str.length > 80 ? ' ...' : ''}`);
+  process.exit();
+}
 
+const reName    = new RegExp(/<font.*?id="(\w+?)".*?>/);
+const reHeight  = new RegExp(/<font-face.*?cap-height="(.*?)".*?\/>/s);
 const reGlyph   = new RegExp(/<glyph\s+?(.*?)\/>/g);
 const reUnicode = new RegExp(/unicode="(.)"/);
 
-const output = [];
+const name   = exec(svg, reName,   'name');
+const height = exec(svg, reHeight, 'height');
 
-let glyphExec;
-while (glyphExec = reGlyph.exec(svg)) {
-  const glyph = glyphExec[0];
-  console.log({glyph});
+const output = {name, height};
+console.log(output);
 
-  const unicode = reUnicode.exec(glyph)?.[1];
-  if(!unicode) {
-    console.log('Error, unicode missing', {glyph});
-    break;
-  };
-  console.log({unicode});
-}
+// let glyphExec;
+// while (glyphExec = reGlyph.exec(svg)) {
+//   const glyph = glyphExec[0];
+//   console.log({glyph});
+
+//   const unicode = reUnicode.exec(glyph)?.[1];
+//   if(missingErr(unicode, 'unicode in glyph')) break;
+//   console.log({unicode});
+// }

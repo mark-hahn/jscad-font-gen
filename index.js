e60de1532436aa6ed01f388ab3b27bea0693b1fe
Mark Hahn <mark@hahnca.com>
2022-02-14 17:44:51 -0800

    injecting fonts

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import fs        from 'fs'
 import yargs     from 'yargs'
 import {hideBin} from 'yargs/helpers'
 
+//////////  PROCESS COMMAND-LINE OPTIONS  ///////////
 const argv = yargs(hideBin(process.argv)).argv;
 
 let lettersRegex = argv.l;
-if(!argv.l) lettersRegex = '[\\x00-\\xFF]';
+if(lettersRegex === true) {
+  console.log(`Error: found "-l" but no regex follows`);
+  process.exit();
+}
+if(!argv.l) lettersRegex = '[\\x20-\\x7E]';
 let reLetters;
 try {
   reLetters = new RegExp(lettersRegex);
@@ -16,38 +21,49 @@ catch(e) {
 }
 
 let inputFile = argv.i;
+if(inputFile === true) {
+  console.log(`Error: found "-i" but no file path follows`);
+  process.exit();
+}
 if(!fs.existsSync(inputFile)) {
   if(fs.existsSync('fonts')) {
-    console.log(`Warning: input "${inputFile}" doesn't exist, ` + 
-                `using "fonts/" directory`);
-    inputFile = 'fonts';
+    console.log(`Warning: input "${inputFile}" doesn't exist.`);    inputFile = 'fonts';
   }
   else {
     console.log(`Error: neither "${inputFile}" ` +
-                `nor "fonts/" directory found`);
+                `nor "fonts" directory found`);
     process.exit();
   }
 }
 
+const DEFAULT_OUTPUT = 
+  '/mnt/c/Users/mark/apps/neon/jscad-text-gen/text-gen.js'
 let outputFile = argv.o;
-if(!outputFile || !fs.existsSync(outputFile)) outputFile = 'fonts';
-if(!fs.existsSync(outputFile)) {
-  if(fs.existsSync('fonts')) {
-    console.log(`Warning: output "${outputFile}" doesn't exist, ` + 
-                `using "fonts/" directory`);
-    outputFile = 'fonts';
-  }
-  else {
-    console.log(`Error: neither ${outputFile} ` +
-                `nor fonts directory found`);
-    process.exit();
-  }
+if(outputFile === true) {
+  console.log(`Error: found "-o" but no file path follows`);
+  process.exit();
 }
-console.log(`>>> Using letter regex "${lettersRegex}" ` +
-            `with input "${inputFile}" and output "${outputFile}"`);
+if(!outputFile) outputFile = DEFAULT_OUTPUT;
+else if(!fs.existsSync(outputFile)) {
+  console.log(
+    `Warning: file "${outputFile}" doesn't exist.`);
+  console.log('The output file must exist for injection of font(s).');
+  console.log(`using "${DEFAULT_OUTPUT}"`);
+  outputFile = DEFAULT_OUTPUT;
+}
+if(!fs.existsSync(outputFile)) {
+  console.log(`Error: file ${outputFile} doesn't exit`);
+  process.exit();
+}
+
+console.log(`Using:`+
+          `\n  Regex:  ${lettersRegex}` +
+          `\n  Input:  ${inputFile}` +
+          `\n  Output  ${outputFile}`);
+
+//////////////  FIND FILE PATHS  //////////////
 
 let fontPaths = [];
-
 const walkDir = function(dir) {
   var list = fs.readdirSync(dir);
   list.forEach( (file) => {
@@ -56,16 +72,17 @@ const walkDir = function(dir) {
     if (stat && stat.isDirectory()) { 
       fontPaths = fontPaths.concat(walkDir(file));
     } else { 
-      if(file?.endsWith('.svg')) fontPaths.push(file);
+      if(file && file.endsWith('.svg')) fontPaths.push(file);
     }
   });
 }
-if(fs.existsSync(inputFile)) {
-  console.log(`Error: ${inputFile} not found`);
+if(!fs.existsSync(inputFile)) {
+  console.log(`Error: file/directory ${inputFile} not found`);
   process.exit();
 }
 else if(fs.lstatSync(inputFile).isDirectory()) {
-  console.log(`Scanning input directory ${inputFile}`);
+  console.log(
+    `Scanning input directory ${inputFile} recursively`);
   walkDir(inputFile);
 }
 else {
@@ -75,12 +92,13 @@ else {
     process.exit();
   }
 }
+fontPaths = fontPaths.filter(path => !!path);
 if(fontPaths.length == 0) {
   console.log(`Error: no .svg file found`);
   process.exit();
 }
 
-console.log({fontPaths});
+//////////  REGEX UTILITIES  ///////////
 
 const exec1 = (regex, str, name, dbgOk=false, dbgErr=true) => {
   const group = regex.exec(str)?.[1];
@@ -111,8 +129,17 @@ const reHAdvX    = new RegExp(/horiz-adv-x="([\d\.]*?)"/is);
 const rePoints   = new RegExp(/d="(.*?)"/igs);
 const rePoint    = new RegExp(/([ML])\s+([\d\.-]+)\s+([\d\.-]+)/igs);
 
-for (let fontPath of fontPaths) { 
+//////////  GENERATE OUTPUT TEXT ///////////
 
+const INJECTED_TEXT_INTRO = 
+  '\n//=== Fonts injected by jscad-font-gen ===\n';
+const INJECTED_TEXT_OUTRO = 
+  '//=== End of injected fonts ===\n';
+
+let output = INJECTED_TEXT_INTRO + 'const fonts = {'
+
+for (let fontPath of fontPaths) { 
+  console.log(`Processing ${fontPath} ...`);
   const svg = fs.readFileSync(fontPath).toString();
 
   const name   = exec1(reName,   svg,   'name');
@@ -143,8 +170,14 @@ for (let fontPath of fontPaths) {
       output += '],';
     }
   }
-  output += '},\n';
+  output += '}},\n';
 }
-output += '}';
-console.log(output);
-fs.writeFileSync("fonts/output.js", output);
+output += INJECTED_TEXT_OUTRO;
+
+let fileOut = fs.readFileSync(outputFile).toString();
+const reInjectionStr = INJECTED_TEXT_INTRO + 
+                '.*' + INJECTED_TEXT_OUTRO;
+const reInjection = new RegExp(reInjectionStr,'igs');
+fileOut = fileOut.replace(reInjection,'');
+fileOut += output;
+fs.writeFileSync(outputFile, fileOut);
